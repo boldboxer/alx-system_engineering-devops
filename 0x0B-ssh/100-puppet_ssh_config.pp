8e1feea6aecbdd7c93829e72cc9d4b9114e74359
boldboxer <ebesteinpr@gmail.com>
2023-08-30 07:42:00 -0700

    using puppet

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# Turns off password authentication and uses school key
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+	#ssh client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+}
